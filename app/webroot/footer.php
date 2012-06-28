d5c0a73315804e110f91db041e722c6fb0c687e9
Adrian Soluch <adrian@soluch.at>
2012-06-28 10:06:56 +0200

    retab

--- app/webroot/footer.php
+++ app/webroot/footer.php
@@ -1,7 +1,7 @@
 <div class="container footer">
         <div class="row">
                 <div class="twelvecol">
-					<p style="font-size:10px;float:right;">SocialRouter social distribution tool by Adrian Soluch 2012</p>
+                    <p style="font-size:10px;float:right;">SocialRouter social distribution tool by Adrian Soluch 2012</p>
                 </div>
         </div>
 </div>
--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,9 +1,5 @@
 <?php
 
-//echo '<pre>';
-//print_r($_SERVER);
-//print_r($_GET);
-
 if (!defined('CSS_DIR')) {
     define('CSS_DIR', 'http://' . $_SERVER['SERVER_NAME'] . '/app/webroot/css/');
 }
@@ -22,16 +18,6 @@ $Dispatcher->dispatch();
 if($GLOBALS['renderPiwik']) :
 ?>
     <!-- Piwik -->
-    <script type="text/javascript">
-    /*var pkBaseURL = (("https:" == document.location.protocol) ? "https://www.soluch.at/other/piwik/" : "http://www.soluch.at/other/piwik/");
-    document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));*/
-    </script><script type="text/javascript">
-    /*try {
-    var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2);
-    piwikTracker.trackPageView();
-    piwikTracker.enableLinkTracking();
-    } catch( err ) {}*/
-    </script><noscript><p><img src="http://www.soluch.at/other/piwik/piwik.php?idsite=2" style="border:0" alt="" /></p></noscript>
     <!-- End Piwik Tag -->
 <?php
 endif;
